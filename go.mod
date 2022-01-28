be10821725a8f64b0d2f2c62ac94f28892a5886b
Eric Lagergren <eric@ericlagergren.com>
2022-01-28 00:40:57 -0800

    add stubs
    Signed-off-by: Eric Lagergren <eric@ericlagergren.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ericlagergren/hctr2
+
+go 1.18
---
+++ hctr2.go
@@ -0,0 +1,5 @@
+// Package HCTR2 implements the HCTR2 length-preserving
+// encryption algorithm.
+//
+// [0]: https://eprint.iacr.org/2021/1441
+package hctr2
